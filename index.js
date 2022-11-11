6b12b5e07a62470d89b78613cfe1d1310886f66e
Link 1503 <104648877+Link1503@users.noreply.github.com>
2022-11-11 19:24:42 +0100

    Update index.js
    Made the self destruct to a comment

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ async function init(token) {
             await interaction.createMessage({
                 content: 'To everyone who gets this message: You are perfect as you are, keep trying, you are worth it. There is alway someone who will help and love you <3'
             })
-            console.log('Self destructing...')
-            process.exit(0)
+            #console.log('Self destructing...')
+            #process.exit(0)
         }
     })
     SynthesisTwo.connect();
